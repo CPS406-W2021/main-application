607435321812fea186fd3e86de737f0f712a2a63
Rkuangdev <RKuangDev@gmail.com>
2021-03-23 23:06:58 -0400

    firebase init setup
    Setup dotenv npm with firebase credentials,
    App.js setup for firebase

--- src/App.js
+++ src/App.js
@@ -1,5 +1,42 @@
 import React, { Component } from "react";
 import { BrowserRouter } from "react-router-dom";
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import { createStore, combineReducers, compose } from 'redux'
+import { ReactReduxFirebaseProvider, firebaseReducer } from 'react-redux-firebase'
+
+require('dotenv').config()
+
+const firebaseConfig = {
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID
+}
+
+// react-redux-firebase config
+const rrfConfig = {
+    userProfile: 'users'
+    // useFirestoreForProfile: true // Firestore for Profile instead of Realtime DB
+    // enableClaims: true // Get custom claims along with the profile
+}
+
+
+// Create store with reducers and initial state
+const initialState = {}
+const store = createStore(rootReducer, initialState)
+
+
+const rrfProps = {
+    firebase,
+    config: rrfConfig,
+    dispatch: store.dispatch
+    // createFirestoreInstance // <- needed if using firestore
+}
+
+firebase.initializeApp(firebaseConfig)
 export default class App extends Component {
     render() {
         return (
