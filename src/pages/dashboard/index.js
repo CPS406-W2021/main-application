6ead09e3ea69c6772b55f19a5fd79f22c74be912
Ralph Liton <rj_liton@hotmail.com>
2021-03-25 19:41:14 -0400

    Update index.js
    hi ty

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -2,6 +2,6 @@ import React, { Component } from "react";
 
 export default class index extends Component {
     render() {
-        return <div>Dashboard</div>;
+        return <div>Ralph</div>;
     }
 }
