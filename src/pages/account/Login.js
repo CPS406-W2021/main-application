8e3051c1a007ae1611da1ce8128224d5b160bc2b
Alyanna Santos <alyannamariesantos@gmail.com>
2021-03-27 19:09:49 -0400

    base html for login and register pages

--- src/pages/account/Login.js
+++ src/pages/account/Login.js
@@ -2,6 +2,20 @@ import React, { Component } from "react";
 
 export default class Login extends Component {
     render() {
-        return <div>Login</div>;
+        return <div>
+            <h1>User Login</h1>
+            <div>
+                <div>
+                    <label for="email">Email:</label>
+                    <input type="text" id="email" name="email"></input>
+                </div>
+                <div>
+                    <label for="pwd">Password:</label>
+                    <input type="password" id="pwd" name="pwd"></input>
+                </div>
+                <a href="/portal"><button type="button">Log In</button></a>
+                <p>Don't have an account? <a href="/register">Register now</a> </p>
+            </div>
+        </div>
     }
 }
