6da8233d837aff08621131d8a732d989d37c7e1a
SholehSepehri <sholeh.sepehri.b@gmail.com>
2021-03-28 22:03:23 -0400

    Login Page - In-progress
    I tried working on the login page, but it's not finished yet

--- src/pages/account/Login.js
+++ src/pages/account/Login.js
@@ -1,21 +1,39 @@
 import React, { Component } from "react";
-
+import Image from "../../images/header.png";
 export default class Login extends Component {
     render() {
-        return <div>
-            <h1 className = "User_login">User Login</h1>
-            <div>
-                <div>
-                    <label for="email">Email:</label>
-                    <input type="text" id="email" name="email"></input>
+        return (
+            <div className="wp-con">
+                    <div className={`wp-header ${this.props.clearbg ? "" : "active"}`}>
+
+                        <div className="wp-header__title">
+                            <div className="wp-header__titleT">
+                                <img src={Image} alt="Toronto Logo" />
+                                <div>Cypress</div>
+                            </div>
+
+                        </div>
+                    </div>
+
+                <div className={`wp-con__body ${this.props.clearbg ? "" : "active"}`}>
+                    <h1>User Login</h1>
+                    <div className = "wp-con__body__login-form">
+                        <label for="email">Email</label><br></br>
+                        <input type="text" id="email" name="email"></input><br></br><br></br>
+
+                        <label for="pwd">Password:</label><br></br>
+                        <input type="password" id="pwd" name="pwd"></input>
+                    </div><br></br>
+                    <a href="/portal">Log In</a>
+                    <p>Don't have an account? <a href="/register">Register now</a> </p>
+
                 </div>
-                <div>
-                    <label for="pwd">Password:</label>
-                    <input type="password" id="pwd" name="pwd"></input>
+                <div className="wp-con__footer">
+                    <div className="active">English (En)</div>
+                    <div>Français (Fr)</div>
                 </div>
-                <a href="/portal"><button type="button">Log In</button></a>
-                <p>Don't have an account? <a href="/register">Register now</a> </p>
             </div>
-        </div>
+        );
     }
+
 }
