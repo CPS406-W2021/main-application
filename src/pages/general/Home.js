f931296c9b2d4a1727d76ff8efb5adcee96b206c
Alyanna Santos <72548456+al-yanna@users.noreply.github.com>
2021-03-29 19:32:17 -0400

    added css on slogan and survey #14

--- src/pages/general/Home.js
+++ src/pages/general/Home.js
@@ -1,34 +1,38 @@
 import React, { Component } from "react";
 import DashboarWrapper from "../../components/ThemeWrapper";
+import Image from "../../images/skyline.png";
 
 function Slogan() {
     return (
-        <section>
-            <h1>Keeping Toronto Safe</h1>
-            <i>"Lorem ipsum dolor sit amet, consectetur adipiscing
-            elit, sed do eiusmod <br></br>
-            adipiscing elit, sed do eiusmod tempor incididunt ut
-            labore et dolore magna aliqua.”</i>
+        <section class="slogan-container">
+            <img src={Image} alt="Toronto Skyline"></img>
+            <h1 class="main-slogan">Keeping Toronto Safe</h1>
+            <i class="descr-slogan">"Lorem ipsum dolor sit amet, consectetur adipiscing
+            elit, sed do eiusmod <br></br> tempor incididunt ut labore et dolore magna aliqua.”</i>
+            <div>
+                <a href="/login"><button type="button" class="btn-login">Log In</button></a>
+                <a href="/register"><button type="button" class="btn-signup">Sign Up</button></a>
+            </div>
         </section>
     );
 }
 
 function Survey() {
     return (
-        <section>
-            <p>
+        <div>
+            <p class="survey-text">
                 Something about taking feedback into consideration,
                 blahblahblahblah.<br>
                 </br>Take our survey so we can blahblahblah :D.
                 <a href="/survey">Take Survey</a>
             </p>
-        </section>
+        </div>
     );
 }
 
 function FAQ() {
     return (
-        <section>
+        <div>
             <h1>Frequently Asked Questions (FAQ)</h1>
             <h3>Question 1</h3>
             <p>Answer 1</p>
@@ -36,7 +40,7 @@ function FAQ() {
             <p>Answer 2</p>
             <h3>Question 3</h3>
             <p>Answer 3</p>
-        </section>
+        </div>
     );
 }
 
