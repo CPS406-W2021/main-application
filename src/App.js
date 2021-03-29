157d0f1012c67af91a7b2600c6e89d21f919c1ac
Farhan Mohammed <farhanhm12@gmail.com>
2021-03-29 18:04:17 -0400

    Added files for people to work on

--- src/App.js
+++ src/App.js
@@ -44,6 +44,7 @@ import PortalBase from "./pages/portal";
 import Login from "./pages/account/Login";
 import Register from "./pages/account/Register";
 import RAPBase from "./pages/reportAProblem";
+import TellAFriend from "./pages/dashboard/TellAFriend";
 require("dotenv").config();
 export default class App extends Component {
     render() {
@@ -60,6 +61,9 @@ export default class App extends Component {
                 </Route>
                 <Route path="/rap">
                     <RAPBase />
+                </Route>
+                <Route path="/taf">
+                    <TellAFriend />
                 </Route>
                 <Route path="/contact">
                     <Contact />
