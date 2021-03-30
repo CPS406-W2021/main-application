9c8f55fa3ee528ce81c90e7230f82ad9e5609de3
Farhan Mohammed <farhanhm12@gmail.com>
2021-03-29 21:50:54 -0400

    Setup the react componenet for the React Component page

--- src/pages/reportAProblem/index.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from "react";
-import DashboarWrapper from "../../components/ThemeWrapper";
-
-export default class RAPBase extends Component {
-    render() {
-        return <DashboarWrapper currentPage={2}>RAPBASE!!</DashboarWrapper>;
-    }
-}
