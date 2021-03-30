c60c25d661d4af5e19d90b530a4ceee99df32b4c
Farhan Mohammed <farhanhm12@gmail.com>
2021-03-29 21:10:22 -0400

    Made a Wrapper for the login page

--- src/pages/account/Login.js
+++ src/pages/account/Login.js
@@ -1,39 +1,76 @@
 import React, { Component } from "react";
-import Image from "../../images/header.png";
+import SingePageWrapper from "../../components/SinglePageWrapper";
 export default class Login extends Component {
     render() {
         return (
-            <div className="auth-con">
-                    <div className={`auth-header ${this.props.clearbg ? "" : "active"}`}>
+            <SingePageWrapper>
+                <h1>User Login</h1>
+                <div className="auth-con__body__login-form">
+                    <label for="email" className="auth-label">
+                        Email
+                    </label>
+                    <br></br>
+                    <input
+                        type="text"
+                        id="email"
+                        name="email"
+                        className="auth-input"
+                    ></input>
+                    <br></br>
+                    <br></br>
 
-                        <div className="auth-header__title">
-                            <div className="auth-header__titleT">
-                                <img src={Image} alt="Toronto Logo" />
-                                <div>Cypress</div>
-                            </div>
-
+                    <label for="pwd" className="auth-label">
+                        Password:
+                    </label>
+                    <br></br>
+                    <input
+                        type="password"
+                        id="pwd"
+                        name="pwd"
+                        className="auth-input"
+                    ></input>
+                </div>
+                <br></br>
+                <br></br>
+                <a href="/portal">Log In</a>
+                <p>
+                    Don't have an account? <a href="/register">Register now</a>{" "}
+                </p>
+                {/* 
+                <form class="ui form">
+                    <div class="field">
+                        <label>First Name</label>
+                        <input
+                            type="text"
+                            name="first-name"
+                            placeholder="First Name"
+                        />
+                    </div>
+                    <div class="field">
+                        <label>Last Name</label>
+                        <input
+                            type="text"
+                            name="last-name"
+                            placeholder="Last Name"
+                        />
+                    </div>
+                    <div class="field">
+                        <div class="ui checkbox">
+                            <input
+                                type="checkbox"
+                                tabindex="0"
+                                class="hidden"
+                            />
+                            <label>I agree to the Terms and Conditions</label>
                         </div>
-                    </div><br></br><br></br>
-
-                <div className={`auth-con__body ${this.props.clearbg ? "" : "active"}`}>
-                    <h1>User Login</h1>
-                    <div className = "auth-con__body__login-form">
-                        <label for="email" className = "auth-label">Email</label><br></br>
-                        <input type="text" id="email" name="email" className="auth-input"></input><br></br><br></br>
-
-                        <label for="pwd" className = "auth-label">Password:</label><br></br>
-                        <input type="password" id="pwd" name="pwd" className="auth-input"></input>
-                    </div><br></br><br></br>
-                    <a href="/portal">Log In</a>
-                    <p>Don't have an account? <a href="/register">Register now</a> </p>
-
-                </div>
-                <div className="auth-con__footer">
-                    <div className="active">English (En)</div>
-                    <div>Français (Fr)</div>
-                </div>
-            </div>
+                    </div>
+                    <button class="ui button" type="submit">
+                        Submit
+                    </button>
+                </form>
+            
+             */}
+            </SingePageWrapper>
         );
     }
-
 }
--- src/pages/account/Register.js
+++ src/pages/account/Register.js
@@ -1,33 +1,41 @@
 import React, { Component } from "react";
+import SingePageWrapper from "../../components/SinglePageWrapper";
 
 export default class Login extends Component {
     render() {
-        return <div>
-            <h1>Sign Up</h1>
-            <div>
+        return (
+            <SingePageWrapper>
+                <h1>Sign Up</h1>
                 <div>
-                    <label for="name">Name:</label>
-                    <input type="text" id="name" name="name"></input>
+                    <div>
+                        <label for="name">Name:</label>
+                        <input type="text" id="name" name="name"></input>
+                    </div>
+                    <div>
+                        <label for="usrn">Username:</label>
+                        <input type="text" id="usrn" name="usrn"></input>
+                    </div>
+                    <div>
+                        <label for="email">Email:</label>
+                        <input type="text" id="email" name="email"></input>
+                    </div>
+                    <div>
+                        <label for="pwd">Password:</label>
+                        <input type="password" id="pwd" name="pwd"></input>
+                    </div>
+                    <div>
+                        <label for="pwd">Confirm Password:</label>
+                        <input type="password" id="cpwd" name="cpwd"></input>
+                    </div>
+                    <a href="/login">
+                        <button type="button">Register</button>
+                    </a>
+                    <p>
+                        {" "}
+                        <a href="/">Cancel</a>{" "}
+                    </p>
                 </div>
-                <div>
-                    <label for="usrn">Username:</label>
-                    <input type="text" id="usrn" name="usrn"></input>
-                </div>
-                <div>
-                    <label for="email">Email:</label>
-                    <input type="text" id="email" name="email"></input>
-                </div>
-                <div>
-                    <label for="pwd">Password:</label>
-                    <input type="password" id="pwd" name="pwd"></input>
-                </div>
-                <div>
-                    <label for="pwd">Confirm Password:</label>
-                    <input type="password" id="cpwd" name="cpwd"></input>
-                </div>
-                <a href="/login"><button type="button">Register</button></a>
-                <p> <a href="/">Cancel</a> </p>
-            </div>
-        </div>
+            </SingePageWrapper>
+        );
     }
 }
