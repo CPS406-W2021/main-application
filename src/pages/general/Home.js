e5f2c5110065de51b54a2781b633ec0b2ae0f7f7
Farhan Mohammed <farhanhm12@gmail.com>
2021-03-31 08:23:40 -0400

    Fixed the home page

--- src/pages/general/Home.js
+++ src/pages/general/Home.js
@@ -4,27 +4,31 @@ import DashboarWrapper from "../../components/ThemeWrapper";
 const FAQInfo = [
     {
         q: "What types of problems should I report to CyPress?",
-        a: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
+        a:
+            '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
     },
     {
         q: "How long does reported problems usually take to be resolved?",
-        a: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
+        a:
+            '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
     },
     {
         q: "How can I directly contact city officials about my problem?",
-        a: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
+        a:
+            '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
     },
 ];
 
 class FAQSingle extends Component {
     constructor(props) {
         super(props);
-        this.state = { open: true };
+        this.state = { open: false };
     }
     render() {
         return (
             <div class="faq-item">
-                <div class="faq-item-header"
+                <div
+                    class="faq-item-header"
                     onClick={() => {
                         this.setState({ open: !this.state.open });
                     }}
@@ -45,21 +49,18 @@ export default class Home extends Component {
             <div class="slogan-con">
                 <h1 class="main-slogan">Keeping Toronto Safe</h1>
                 <i class="descr-slogan">
-                    The CyPress system is designed to allow you to report problems
-                    that concern <br></br>you about the city. Our mission is to improve
-                    the city's public safety <br></br>through the contibutions of our citizens.
+                    The CyPress system is designed to allow you to report
+                    problems that concern you about the city. Our mission is to
+                    improve the city's public safety through the contibutions of
+                    our citizens.
                 </i>
-                <div>
-                    <a href="/login">
-                        <button type="button" class="btn-login">
-                            Log In
-                        </button>
-                    </a>
-                    <a href="/register">
-                        <button type="button" class="btn-signup">
-                            Sign Up
-                        </button>
-                    </a>
+                <div class="slogan-btn__con">
+                    <button type="button" class="btn-login">
+                        Log In
+                    </button>
+                    <button type="button" class="btn-signup">
+                        Sign Up
+                    </button>
                 </div>
             </div>
         );
@@ -69,13 +70,10 @@ export default class Home extends Component {
         return (
             <div>
                 <p class="survey-text">
-                    We take feedback from our users seriously. This survey
-                    will allow us
-                    <br></br>
-                    to continously improve our system according
-                    to our users needs.
-                    <br></br>
-                    This will take approximately 5 minutes to complete.
+                    We take feedback from our users seriously. This survey will
+                    allow us to continously improve our system according to our
+                    users needs. This will take approximately 5 minutes to
+                    complete.
                     <a href="/survey"> Take Survey </a>
                 </p>
             </div>
