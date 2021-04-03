12f05fed87ea8db8ee0892b3b10939880e9d99d0
Farhan <farhanhm12@gmail.com>
2021-04-02 21:15:52 -0400

    Fixed merge issue

--- src/pages/account/Login.js
+++ src/pages/account/Login.js
@@ -1,44 +1,37 @@
-import React, { Component } from "react";
-import SingePageWrapper from "../../components/SinglePageWrapper";
-export default class Login extends Component {
-import { connect } from 'react-redux'
-import { signIn } from '../../store/actions/authActions'
+import React, { Component } from 'react';
+import SingePageWrapper from '../../components/SinglePageWrapper';
+import { connect } from 'react-redux';
+import { signIn } from '../../store/actions/authActions';
 
 class Login extends Component {
     state = {
         email: '',
-        password: ''
-    }
+        password: '',
+    };
     handleSubmit = (e) => {
         e.preventDefault();
-        this.props.login(this.state)
-    }
+        this.props.login(this.state);
+    };
     render() {
         return (
             <SingePageWrapper>
                 <div className="login-form ui small form">
                     <h1>User Login</h1>
 
-                    <label for="email">
-                        Email
-                    </label>
+                    <label for="email">Email</label>
                     <br></br>
-                    <input type="text" id="email" name="email" ></input><br></br>
-
-
-                    <label for="pwd">
-                        Password:
-                    </label>
+                    <input type="text" id="email" name="email"></input>
                     <br></br>
-                    <input
-                        type="password"
-                        id="pwd"
-                        name="pwd"
-                    ></input>
+
+                    <label for="pwd">Password:</label>
+                    <br></br>
+                    <input type="password" id="pwd" name="pwd"></input>
                     <button class="ui yellow button">LOG IN</button>
-                    <p> Don't have an account? <a href="/register">Register now</a>{" "} </p>
+                    <p>
+                        {' '}
+                        Don't have an account? <a href="/register">Register now</a>{' '}
+                    </p>
                 </div>
-
             </SingePageWrapper>
         );
     }
@@ -46,12 +39,12 @@ class Login extends Component {
 
 const mapStateToProps = (state) => {
     return {
-        authError: state.auth.authError
-    }
-}
+        authError: state.auth.authError,
+    };
+};
 const mapDispatchToProps = (dispatch) => {
     return {
-        login: (creds) => dispatch(signIn(creds))
-    }
-}
-export default connect(mapStateToProps, mapDispatchToProps)(Login)
+        login: (creds) => dispatch(signIn(creds)),
+    };
+};
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
