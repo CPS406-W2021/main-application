18807e1fc4bb6c6d09ac2125d485df9180a27bba
Farhan Mohammed <44536753+farhan-mohammed@users.noreply.github.com>
2021-04-06 13:08:35 -0400

    Mapbox setup (#36)
    * Added Mapbox base
    
    * Added marker images

--- src/pages/portal/index.js
+++ src/pages/portal/index.js
@@ -1,74 +1,93 @@
-import React, { Component } from "react";
-import DashboarWrapper from "../../components/ThemeWrapper";
+import React, { Component } from 'react';
+import DashboarWrapper from '../../components/ThemeWrapper';
+import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 export default class PortalBase extends Component {
     render() {
         const dummyData = [
             {
-                type: "Pothole",
-                address: "123 Street St.",
-                date: "3/21/2021 11:24AM",
+                type: 'Pothole',
+                address: '123 Street St.',
+                date: '3/21/2021 11:24AM',
             },
             {
-                type: "Pothole",
-                address: "123 Street St.",
-                date: "3/21/2021 11:24AM",
+                type: 'Pothole',
+                address: '123 Street St.',
+                date: '3/21/2021 11:24AM',
             },
             {
-                type: "Pothole",
-                address: "123 Street St.",
-                date: "3/21/2021 11:24AM",
+                type: 'Pothole',
+                address: '123 Street St.',
+                date: '3/21/2021 11:24AM',
             },
             {
-                type: "Pothole",
-                address: "123 Street St.",
-                date: "3/21/2021 11:24AM",
+                type: 'Pothole',
+                address: '123 Street St.',
+                date: '3/21/2021 11:24AM',
             },
             {
-                type: "Pothole",
-                address: "123 Street St.",
-                date: "3/21/2021 11:24AM",
+                type: 'Pothole',
+                address: '123 Street St.',
+                date: '3/21/2021 11:24AM',
             },
             {
-                type: "Pothole",
-                address: "123 Stret St.",
-                date: "3/21/2021 11:24AM",
+                type: 'Pothole',
+                address: '123 Stret St.',
+                date: '3/21/2021 11:24AM',
             },
             {
-                type: "Pothole",
-                address: "123 Street St.",
-                date: "3/21/2021 11:24AM",
+                type: 'Pothole',
+                address: '123 Street St.',
+                date: '3/21/2021 11:24AM',
             },
             {
-                type: "Pothole",
-                address: "123 Street St.",
-                date: "3/21/2021 11:24AM",
+                type: 'Pothole',
+                address: '123 Street St.',
+                date: '3/21/2021 11:24AM',
             },
             {
-                type: "Pothole",
-                address: "123 Street St.",
-                date: "3/21/2021 11:24AM",
+                type: 'Pothole',
+                address: '123 Street St.',
+                date: '3/21/2021 11:24AM',
             },
             {
-                type: "Pothole",
-                address: "123 Street St.",
-                date: "3/21/2021 11:24AM",
+                type: 'Pothole',
+                address: '123 Street St.',
+                date: '3/21/2021 11:24AM',
             },
             {
-                type: "Pothole",
-                address: "123 Street St.",
-                date: "3/21/2021 11:24AM",
+                type: 'Pothole',
+                address: '123 Street St.',
+                date: '3/21/2021 11:24AM',
             },
             {
-                type: "Pothole",
-                address: "123 Street St.",
-                date: "3/21/2021 11:24AM",
+                type: 'Pothole',
+                address: '123 Street St.',
+                date: '3/21/2021 11:24AM',
             },
         ];
+        const Map = ReactMapboxGl({
+            accessToken:
+                'pk.eyJ1IjoiZmFyaGFuaG0iLCJhIjoiY2tuMTUxYjNnMHIyODJvbzJueDJzdWJmcCJ9.EIl7ZcqlshPyJxnxyGNGhg',
+        });
         return (
             <DashboarWrapper currentPage={1} clearbg={true}>
                 <div className="portal-con">
-                    <div className="portal-button">Report</div>
+                    <Map
+                        style="mapbox://styles/mapbox/streets-v9"
+                        containerStyle={{
+                            flex: 1,
+                            borderRadius: 35,
+                        }}
+                        center={[-79.3788, 43.6577]}
+                        zoom={[16]}
+                    >
+                        <Layer type="symbol" id="marker" layout={{ 'icon-image': 'marker-15' }}>
+                            <Feature coordinates={[-79.3788, 43.6577]} />
+                        </Layer>
+                    </Map>
+                    ;<div className="portal-button">Report</div>
                     <div className="portal-search">
                         <div className="portal-search__header">
                             <div>Recent Activity </div>
