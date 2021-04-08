6478d00ae67ca186223dac98ccd2b8ca42167b8a
Rkuangdev <RKuangDev@gmail.com>
2021-04-08 11:01:11 -0400

    Firebase setup for surveys

--- src/store/actions/surveryActions.js
+++ src/store/actions/surveryActions.js
@@ -1,10 +1,17 @@
 export const enterSurvey = (entries) => {
     return (dispatch, getState, getFirebase) => {
-        const firebase = getFirebase();
+        const firebase = getFirebase().firestore();
         // add to survey databse
         // No need to attach user account to survey
         // [1,2,3,4,5,6] <- Sample entry
-        console.log("called");
-        dispatch({ type: "SURVEY_REGISTRATION_SUCCESSFUL" });
+        firebase
+            .collection('survey')
+            .add({'answers': entries})
+            .then(() => {
+                dispatch({ type: "SURVEY_REGISTRATION_SUCCESSFUL" });
+            })
+            .catch((err) => {
+                dispatch({ type: "SURVEY_REGISTRATION_ERROR" });
+            })
     };
 };
