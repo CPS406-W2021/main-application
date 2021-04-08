2f8ecd1008aad7565be8180e148a0c89a5091738
Ralph Liton <rj_liton@hotmail.com>
2021-04-08 16:36:16 -0400

    Signaler un problème.js is finished oui oui

--- src/pages/dashboard/ReportAProblem.js
+++ src/pages/dashboard/ReportAProblem.js
@@ -1,27 +1,56 @@
 import React, { Component } from "react";
 import DashboarWrapper from "../../components/ThemeWrapper";
+import { connect } from "react-redux";
+{/* <h1 className="main-slogan">
+{L === "en"
+    ? "Keeping Toronto Safe"
+    : "Assurer la sécurité de Toronto"}
+</h1> */}
 
-export default class ReportAProblem extends Component {
+class ReportAProblem extends Component {
     render() {
+        const L = this.props.lang;
         return (
             <DashboarWrapper currentPage={2}>
                 <div className="rap-con">
                     <div className="rap-map"></div>
                     <div className="rap-body">
-                        <h1 className="rap-h1">Report a Problem</h1>
-                        <div className="rap-sub">at 123 St. Street St.</div>
-
+                        <h1 className="rap-h1">
+                            { L === "en"
+                                ? "Report a Problem"
+                                : "Signaler un problème" }
+                        </h1>
+                        
+                        <div className="rap-sub">
+                            { L === "en"
+                                ? "at 123 St. Street"
+                                : "au 123 St. Street St" }
+                            </div>
                         <form class="ui form">
                             <div class="field">
-                                <label>Problem at Site</label>
+                                <label>
+                                    { L === "en"
+                                        ? "Problem at Site"
+                                        : "Problème sur le site" }
+                                </label>
                                 <div class="ui form">
                                     <div class="field">
                                         <select>
                                             <option value="-1">
-                                                Select an Issue
+                                                { L === "en"
+                                                    ? "Select an Issue"
+                                                    : "Sélectionnez un problème" }
                                             </option>
-                                            <option value="1">Pothole</option>
-                                            <option value="0">Tree</option>
+                                            <option value="1">
+                                                { L === "en"
+                                                    ? "Pothole"
+                                                    : "Nid de poule" }
+                                            </option>
+                                            <option value="0">
+                                                { L === "en"
+                                                    ? "Tree"
+                                                    : "Arbre" }
+                                            </option>
                                         </select>
                                     </div>
                                 </div>
@@ -34,20 +63,31 @@ export default class ReportAProblem extends Component {
                                         class="hidden"
                                     />
                                     <label>
-                                        I want to receive updates on this
-                                        report:
+                                        { L === "en"
+                                            ? "I want to receive updates on this report"
+                                            : "Je souhaite recevoir des mises à jour sur ce rapport" }
                                     </label>
                                 </div>
                             </div>
                             <div class="field">
-                                <label>Enter more information:</label>
+                                <label>
+                                    { L === "en"
+                                        ? "Enter more information"
+                                        : "Entrez plus d'informations" }
+                                </label>
                                 <textarea></textarea>
                             </div>
                             <hr className="rap-line"></hr>
                             <div className="rap-buttons">
-                                <button class="ui green button">Submit</button>
+                                <button class="ui green button">
+                                    { L === "en"
+                                        ? "Submit"
+                                        : "Nous faire parvenir" }
+                                </button>
                                 <button class="ui button" type="submit">
-                                    Cancel
+                                    { L === "en"
+                                        ? "Cancel"
+                                        : "Annuler" }
                                 </button>
                             </div>
                         </form>
@@ -57,3 +97,12 @@ export default class ReportAProblem extends Component {
         );
     }
 }
+
+
+const mapStateToProps = (state) => ({
+    lang: state.lang.lang,
+});
+
+const mapDispatchToProps = {};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ReportAProblem);
